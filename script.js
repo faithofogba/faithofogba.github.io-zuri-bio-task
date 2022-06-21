c82b2afd9248914766e379c7944e5741333c17f6
faithofogba <onomeofogba13@gmail.com>
2022-06-22 00:38:44 +0100

    add bio

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById("demo").innerHTML = 5 + 6;
+let person = {
+    name: "Onome",
+    height: "4ft",
+    country: "Nigeria"
+
+}
+
+document.getElementById("name").innerHTML = person.name + " " + person.height+ " "  + person.country;
+document.getElementById("height").innerHTML = "Height: 4.6ft";
+document.getElementById("country").innerHTML = "Country: Nigeria";
